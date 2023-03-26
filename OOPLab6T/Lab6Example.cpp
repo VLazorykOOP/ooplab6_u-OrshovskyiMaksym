0d3256a4a198fa10143735b15ebbdafe2a737cb9
OrshovskyiMaksym <112846338+OrshovskyiMaksym@users.noreply.github.com>
2023-03-26 16:34:41 +0300

    task3

--- OOPLab6T/Lab6Example.cpp
+++ OOPLab6T/Lab6Example.cpp
@@ -24,6 +24,8 @@ void Example()
 
 		switch (ch) {
 		case '1':  SpaceExample1::mainExample1(#include <iostream>
+//task2
+
 using namespace std;
 
 class Employee {
@@ -83,7 +85,84 @@ int main() {
     return 0;
 }
 );   break;
-		case '2':  SpaceExample2::mainExample2();   break;
+		case '2':  SpaceExample2::mainExample2(
+
+
+//task3
+
+// Базовий клас "Водойма"
+class WaterBody {
+public:
+    WaterBody() {}
+    virtual ~WaterBody() {}
+
+    virtual void printInfo() const {
+        std::cout << "This is a water body." << std::endl;
+    }
+};
+
+// Клас "Бухта", що успадковується від "Водойми"
+class Bay : public WaterBody {
+public:
+    Bay(const std::string& name) : m_name(name) {}
+    virtual ~Bay() {}
+
+    virtual void printInfo() const override {
+        std::cout << "This is a bay called " << m_name << "." << std::endl;
+    }
+
+private:
+    std::string m_name;
+};
+
+// Клас "Залив", що успадковується від "Бухти"
+class Gulf : public Bay {
+public:
+    Gulf(const std::string& name) : Bay(name) {}
+    virtual ~Gulf() {}
+
+    virtual void printInfo() const override {
+        std::cout << "This is a gulf called " << m_name << "." << std::endl;
+    }
+};
+
+// Клас "Море", що успадковується від "Водойми"
+class Sea : public WaterBody {
+public:
+    Sea(const std::string& name) : m_name(name) {}
+    virtual ~Sea() {}
+
+    virtual void printInfo() const override {
+        std::cout << "This is a sea called " << m_name << "." << std::endl;
+    }
+
+private:
+    std::string m_name;
+};
+
+int main() {
+    // Створюємо об'єкти класів і викликаємо метод printInfo()
+    WaterBody* water = new WaterBody();
+    Bay* bay = new Bay("San Francisco Bay");
+    Gulf* gulf = new Gulf("Gulf of Mexico");
+    Sea* sea = new Sea("Mediterranean Sea");
+
+    water->printInfo(); // This is a water body.
+    bay->printInfo(); // This is a bay called San Francisco Bay.
+    gulf->printInfo(); // This is a gulf called Gulf of Mexico.
+    sea->printInfo(); // This is a sea called Mediterranean Sea.
+
+    // Звільняємо пам'ять
+    delete water;
+    delete bay;
+    delete gulf;
+    delete sea;
+
+    return 0;
+}
+
+
+);   break;
 		case '3':  SpaceExample3::mainExample3();   break;
 		case '4':  return;
 		case 'e':  return;
